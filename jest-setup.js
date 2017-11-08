808214baf8eb3de74e30b53ce9f26803027565d4
schava <schava@logitech.com>
2017-11-08 17:05:06 +0530

    Mock Native modules for test
    We need to mock Native modules for jest testing

---
+++ jest-setup.js
@@ -0,0 +1,33 @@
+// @flow
+
+import { NativeModules } from 'react-native';
+
+// monkey patching the locale to avoid the error:
+//  Something went wrong initializing the native ReactLocalization module
+NativeModules.ReactLocalization = {
+    language: "en_US",
+};
+
+
+jest.mock('react-native-code-push', () => {
+    const cp = (_: any) => (app: any) => app;
+    Object.assign(cp, {
+        InstallMode: {},
+        CheckFrequency: {},
+        SyncStatus: {},
+        UpdateState: {},
+        DeploymentStatus: {},
+        DEFAULT_UPDATE_DIALOG: {},
+
+        checkForUpdate: jest.fn(),
+        codePushify: jest.fn(),
+        getConfiguration: jest.fn(),
+        getCurrentPackage: jest.fn(),
+        getUpdateMetadata: jest.fn(),
+        log: jest.fn(),
+        notifyAppReady: jest.fn(),
+        notifyApplicationReady: jest.fn(),
+        sync: jest.fn(),
+    });
+    return cp;
+});
