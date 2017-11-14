ca8f0ce5764b0cfa028fa76c494e4779e47e0fb0
schava <schava@logitech.com>
2017-11-14 09:51:32 +0530

    ci_test added for buddybuild
    ci_test added for buddybuild

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,8 @@
 # Jest won't create our folder structure for us, so lets create it first
-mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
+mkdir buddybuild_artifacts
 
-jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
+mkdir buddybuild_artifacts/Jest
+
+#jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
 # Some users might want to just run
-npm test --coverage #instead, depending on how they have everything set up
+npm ci_test #instead, depending on how they have everything set up
